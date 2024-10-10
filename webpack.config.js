658db7500f273604ce3128e4f59092170f26f8d0
AMEC (IS) Pathanapong Sokpukeaw <patanapo@MitsubishiElevatorAsia.co.th>
2024-10-10 13:27:31 +0700

    add scss

--- webpack.config.js
+++ webpack.config.js
@@ -2,44 +2,45 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-   // กำหนดหลาย entry points
-   entry: {
-    login: './src/login.js',
-    index: './src/index.js'
-  },
-  output: {
-    // ใช้ [name] เพื่อให้ Webpack ตั้งชื่อไฟล์ตาม entry point
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-    clean: true,
-  },
-  mode: 'production',
-  module: {
-    rules: [
-      // กฎสำหรับไฟล์ CSS
-      {
-        test: /\.css$/, // จับเฉพาะไฟล์ .css
-        use: ['style-loader', 'css-loader'], // ใช้ style-loader และ css-loader
-      },
-      // กฎสำหรับไฟล์ SCSS
-      {
-        test: /\.scss$/, // จับเฉพาะไฟล์ .scss
-        use: ['style-loader', 'css-loader', 'sass-loader'], // ใช้ style-loader, css-loader, และ sass-loader
-      },
-    ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.html', // ใช้ไฟล์ HTML ที่เราเพิ่งสร้าง
-      filename: 'index.html',
-    }),
-  ],
-  devServer: {
-    static: {
-      directory: path.join(__dirname, 'dist'),
+    // กำหนดหลาย entry points
+    entry: {
+        login: './src/login.js',
+        index: './src/index.js',
+        item: './src/item.js'
     },
-    compress: true,
-    port: 3000,
-    open: true,
-  },
+    output: {
+        // ใช้ [name] เพื่อให้ Webpack ตั้งชื่อไฟล์ตาม entry point
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+    mode: 'production',
+    module: {
+        rules: [
+            // กฎสำหรับไฟล์ CSS
+            {
+                test: /\.css$/, // จับเฉพาะไฟล์ .css
+                use: ['style-loader', 'css-loader'], // ใช้ style-loader และ css-loader
+            },
+            // กฎสำหรับไฟล์ SCSS
+            {
+                test: /\.scss$/, // จับเฉพาะไฟล์ .scss
+                use: ['style-loader', 'css-loader', 'sass-loader'], // ใช้ style-loader, css-loader, และ sass-loader
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html', // ใช้ไฟล์ HTML ที่เราเพิ่งสร้าง
+            filename: 'index.html',
+        }),
+    ],
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'dist'),
+        },
+        compress: true, // เปิดการบีบอัด
+        port: 3000, // พอร์ตที่เซิร์ฟเวอร์จะรัน
+        open: true // เปิดเบราว์เซอร์โดยอัตโนมัติ
+    }
 };
